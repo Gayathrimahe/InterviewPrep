705c4dd03cfb314dbc01707550f614d6772d04b0
Gayathri <c.keerthis@gmail.com>
2022-07-16 22:03:31 -0400

    commit

---
+++ finding_occ.py
@@ -0,0 +1,13 @@
+
+#finding occurences of string in a list
+def finding_occ():
+    #list1 = ["mon", "tues", "mon", "wed", "thurs", "mon", "fri", "sun", "wed"]
+    #print(list1.count(x))
+    x = input("Enter the list with string elements:")
+    print(x)
+    find_string = input("enter the string to find its count")
+    print(x.count(find_string))
+
+
+
+finding_occ()

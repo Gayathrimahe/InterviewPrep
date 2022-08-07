7e908c5d733c93124c6b3786e89477fb7c350a66
Gayathri <c.keerthis@gmail.com>
2022-08-06 23:36:37 -0400

    commit

--- sort_list.py
+++ sort_list.py
@@ -23,7 +23,25 @@ class Sort_List:
         print(data_list)
 
 
+    def reverse_list(self, new_list):
+        self.new_list = new_list
+        print(new_list[:: -1])
+
+
+    def reverse_through_loop(self, r_list):
+        self.r_list = r_list
+        length = len(r_list)
+        rev_list = []
+        for x in r_list:
+            rev_list.append(r_list[length-1])
+            length -= 1
+        print(rev_list)
+
+
+
 
 test = Sort_List()
 test.sort_list_elements([3, 7, 5, 9, 0])
 test.sort_list_by_loop([89, 45, 23, 99, 55])
+test.reverse_list([54, 3, 5, 7, 11])
+test.reverse_through_loop([9, 7, 3, 5, 1])
